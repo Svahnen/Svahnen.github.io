cb55c33b6064a7245209ea8f16a4ca7f69504b30
Svahnen <svahn.jonny@gmail.com>
2017-10-11 12:10:49 +0200

    Version 2.0

--- app.js
+++ app.js
@@ -1,23 +1,81 @@
-var news = document.getElementById('news');
+// Set variebles for each id
+let p1 = document.getElementById("p1");
+let p2 = document.getElementById("p2");
+let p3 = document.getElementById("p3");
+let p4 = document.getElementById("p4");
+let p5 = document.getElementById("p5");
+let t1 = document.getElementById("t1");
+let t2 = document.getElementById("t2");
+let t3 = document.getElementById("t3");
+let t4 = document.getElementById("t4");
+let t5 = document.getElementById("t5");
+let i1 = document.getElementById("i1");
+let i2 = document.getElementById("i2");
+let i3 = document.getElementById("i3");
+let i4 = document.getElementById("i4");
+let i5 = document.getElementById("i5");
+let sB = document.getElementById("submit");
+let cB = document.getElementById("close");
 
-news.onclick = function() {
-  document.getElementById("p1").innerHTML = "Dagens nyheter är:";
-  document.getElementById("i1").style.display = "inherit";
-  document.getElementById("i1").style.height = "200px";
-  document.getElementById("i1").style.width = "400px";
-  document.getElementById("submit").style.display = "inherit";
-
+// Reset function for the close button and for use on the first line of each form function
+let reset = function(){
+  for(i = 1; i <= 5; i++) {
+    this["p"+i].innerHTML ="";
+    this["t"+i].style.display ="none";
+    this["t"+i].style.height = "auto";
+    this["t"+i].style.width = "auto";
+    this["i"+i].style.display ="none";
+    this["i"+i].style.type ="text";
+    sB.style.display = "none";
+    cB.style.display = "none";
+  }
 };
 
-var feedback = document.getElementById('feedback');
+// Alert to be sent on submit
+let sent = function() {
+  alert("Inskickat!");
+};
 
-feedback.onclick = function() {
-  document.getElementById("p1").innerHTML = "Hur var din dag idag?";
-  document.getElementById("i1").style.display = "inherit";
-  document.getElementById("i1").style.width = "400px";
-  document.getElementById("p2").innerHTML = "Vad åt du till lunch?";
-  document.getElementById("i2").style.display = "inherit";
-  document.getElementById("i2").style.width = "400px";
-  document.getElementById("submit").style.display = "inherit";
+// Close button
+document.getElementById('close').onclick = function() {
+  reset();
+};
 
+// After this line comes all the form functions
+document.getElementById('news').onclick = function() {
+  reset();
+  cB.style.display = "inherit";
+  p1.innerHTML = "Dagens nyheter är:";
+  t1.style.display = "inherit";
+  t1.style.height = "200px";
+  t1.style.width = "400px";
+  t1.required = true;
+  sB.style.display = "inherit";
+};
+
+document.getElementById('feedback').onclick = function() {
+  reset();
+  cB.style.display = "inherit";
+  p1.innerHTML = "Vad har varit bra/dåligt under dagen?";
+  t1.style.display = "inherit";
+  t1.style.width = "400px";
+  t1.required = true;
+  p2.innerHTML = "Hur har klimatet i klassen varit?";
+  t2.style.display = "inherit";
+  t2.style.width = "400px";
+  t2.required = true;
+  sB.style.display = "inherit";
+};
+
+document.getElementById('weekeval').onclick = function() {
+  reset();
+  cB.style.display = "inherit";
+  p1.innerHTML = "Ditt namn (frivilligt)";
+  t1.style.display = "inherit";
+  t1.style.width = "400px";
+  p2.innerHTML = "Hur upplever du arbetsmiljön?";
+  i2.style.display = "inherit";
+  i2.type = "range";
+  i2.style.width = "400px";
+  sB.style.display = "inherit";
 };
