8f59b13f4304033c7e7210c5a2109df720f032d4
Jonny Svahn <svahn.jonny@gmail.com>
2017-11-10 14:39:45 +0100

    Find and apply a solution for making the page seem smooth when scrolling past the navbar

--- Script/app.js
+++ Script/app.js
@@ -1,11 +1,14 @@
-// Make function for setting nav to fixed-top
+// Make function for setting nav to fixed-top, and set paddingTop to the carousel
+// aswell as setting the padded area to the same color as the navbar.
 let navTop = function () {
   if (window.scrollY >= window.innerHeight) {
     document.getElementById('nav-main').className = 'navbar navbar-expand-lg navbar-dark bg-dark fixed-top'
     document.getElementById('carousel').style.paddingTop = '56px'
+    document.getElementById('carousel').style.background = 'linear-gradient(#353a40 56px,#fff 0%)'
   } else {
     document.getElementById('nav-main').className = 'navbar navbar-expand-lg navbar-dark bg-dark'
     document.getElementById('carousel').style.paddingTop = '0'
+    document.getElementById('carousel').style.background = '#fff'
   }
 }
 
